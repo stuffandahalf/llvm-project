54f0b35f8106e29adef202561c6dd57fdbfe4875
Gregory Norton <gregory.norton@me.com>
2020-08-19 03:10:04 -0400

    Corrected ALiX toolchain comments

--- clang/lib/Driver/ToolChains/ALiX.cpp
+++ clang/lib/Driver/ToolChains/ALiX.cpp
@@ -1,4 +1,4 @@
-//===--- Fuchsia.cpp - Fuchsia ToolChain Implementations --------*- C++ -*-===//
+//===------- ALiX.cpp - ALiX ToolChain Implementations ----------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
